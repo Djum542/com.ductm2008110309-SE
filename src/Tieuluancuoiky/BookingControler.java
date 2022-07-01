81530d250bbb11870036458faa12327e6ad0403f
Djum542 <djum54022@gmail.com>
2022-07-01 14:14:19 +0700

    tiểu luận cuối kì

--- src/Tieuluancuoiky/BookingControler.java
+++ src/Tieuluancuoiky/BookingControler.java
@@ -1,21 +1,30 @@
 package Tieuluancuoiky;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import com.google.gson.JsonArray;
 import com.google.gson.JsonObject;
 
+import Tieuluancuoiky.Datphong.BookroomEntity;
 import javafx.event.EventType;
 import javafx.scene.input.MouseButton;
 import javafx.scene.input.MouseEvent;
 import javafx.scene.input.PickResult;
 
 public class BookingControler {
+    private static final JsonArray memory = null;
     private RoomHotel roomHotel;
+
+    public BookingControler(RoomHotel roomHotel) {
+        this.roomHotel = roomHotel;
+    }
 
     public RoomHotel getdiachi() {
         return roomHotel;
     }
 
-    public void bookroom(String diachi) {
+    public void bookroom(String diachi, String sophong) {
         String tempMemory = RoomHotel.getDiachi();
         int index = -1;
         // 1. Đặt phòng
@@ -26,6 +35,30 @@ public class BookingControler {
             System.out.println("Bạn đã đặt phòng ở đây rồi, bạn có muốn đặt thêm không?");
 
         }
+        List<Object> listcheck;
+        listcheck = roomValid(sophong);
+        if (!(boolean) listcheck.get(0)) {
+            System.out.println(listcheck.get(1));
+            (BookroomEntity.getNewRoom()).update(sophong);
+            (BookroomEntity.getNewRoom()).write();
+        } else {
+            System.out.println(listcheck.get(-1));
+        }
+    }
+
+    public static List<Object> roomValid(String sophong) {
+        StroredFile stroredFile = new StroredFile("room.json", memory);
+        stroredFile.read();
+        System.out.println("");
+        List<Object> list = new ArrayList<>();
+        int index = -1;
+        index = (BookroomEntity.getNewRoom()).search("NU", sophong);
+        if (index == -1) {
+            list.add(false);
+            list.add("[Don't have any room] dia diem nay da het phong");
+            return list;
+        }
+        return list;
     }
 
     @Override
--- src/Tieuluancuoiky/Datphong/CreateRoom.java
+++ src/Tieuluancuoiky/Datphong/CreateRoom.java
@@ -7,11 +7,13 @@ import java.util.List;
 import com.google.gson.Gson;
 
 import Tieuluancuoiky.RoomHotel;
+import Tieuluancuoiky.StroredFile;
 
 public class CreateRoom {
     private static final String room = null;
 
     public static void main(String[] args) {
+        
         RoomHotel roomHotel = new RoomHotel();
         roomHotel.addProperty(new RoomHotel(1000000, 80, "254 Dương an, 24, Tan Cu, Da Nang", "true", "5A16"));
         roomHotel.addProperty(new RoomHotel(850000, 60, "13 Chu văn an, 22, Tan Cu, Da Nang ", "false", "12B2"));
@@ -23,6 +25,7 @@ public class CreateRoom {
 
         List<RoomHotel> list = new ArrayList<>();
         list.add(roomHotel);
+        
         Gson gson = new Gson();
         try (FileWriter writer = new FileWriter("room.json")) {
             gson.toJson(list, writer);
